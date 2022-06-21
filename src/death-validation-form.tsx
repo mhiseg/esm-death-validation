919e72d897d4842099afeb11711f0b2a78720ddf
Aristilde Schneider <lerenito.as@gmail.com>
2022-06-21 12:40:58 -0400

    Added cause label for validation form

--- src/death-validation-form.tsx
+++ src/death-validation-form.tsx
@@ -5,6 +5,8 @@ import { Formik } from "formik";
 import { Grid, Row, Column, Button, Form } from "carbon-components-react";
 import { useTranslation } from "react-i18next";
 import { PassField } from "./components/death-form/field/password/password";
+import { Icon } from '@iconify/react';
+import InfoCard from "./components/info-card/info-card";
 
 
 
@@ -36,24 +38,40 @@ const DeathValidation = () => {
                 return (
                     <Form name="form" className={styles.cardForm} onSubmit={handleSubmit}>
                         <Grid fullWidth={true} className={styles.p0}>
-                            <Row>
+                            <Column className={styles.separator}>
+                                <h4 className={styles.title}>{t("cardValidationTitle","Résumé du décès")}</h4>
+                            </Column>
+
+                            <Column className={styles.main}>
+                                <Row >
+                                    <Column className={styles.column1} lg={1}><Icon icon="fa-solid:hospital" className={styles.icon} /></Column>
+                                    <Column className={styles.column2} lg={11}><p className={styles.font}>Notre Dame S.A</p></Column>
+                                </Row>
+                                <Row>
+                                    <Column className={styles.column1} lg={1}><Icon icon="bxs:time-five" className={styles.icon} /></Column>
+                                    <Column className={styles.column2} lg={11}><p className={styles.font}>23h45</p></Column>
+                                </Row>
+                                <Row>
+                                    <Column className={styles.column1} lg={1}><Icon icon="clarity:date-solid" className={styles.icon} /></Column>
+                                    <Column className={styles.column2} lg={11}><p className={styles.font}>12/01/2010</p></Column>
+                                </Row>
+                                <Row>
+                                    <Column className={styles.column1} lg={1}><Icon icon="fa-solid:sticky-note" className={styles.icon} /></Column>
+                                    <Column className={styles.column2} lg={11}><p className={styles.font}>Projectile</p></Column>
+                                </Row>
+                            </Column>
+
+                            <Row className={styles.infoCard}>
                                 <Column>
-                                    <span>Date</span>
+                                    <InfoCard title={t("causeIntermLabel","Cause intermediaire")} info="weruhgruhuiogirgbierbeivseiuefhvbisersrae" />
                                 </Column>
-                                <Column><span>Time</span></Column>
-                            </Row>
-                            <Row>
                                 <Column>
-                                    <span>Lieu de décès</span>
-                                </Column>
-                                <Column>
-                                    <span>Cause principale</span>
+                                    <InfoCard title={t("causeInitLabel","Cause initiale")} info="weruhgruhuiogirgbierbeivseiuefhvbisersrae" />
                                 </Column>
                             </Row>
 
-
                             <Row>
-                                <Column>
+                                <Column className={styles.padingBottom}>
                                     <PassField />
                                 </Column>
 
